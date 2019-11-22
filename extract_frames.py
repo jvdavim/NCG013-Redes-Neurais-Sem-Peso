8cb43707d018496a148ca2731ec1f194f63e5dcc
Joao Victor Almeida Davim <jvadavim@gmail.com>
2019-11-22 09:29:50 -0300

    Finaliza primeira etapa do pipeline

--- extract_frames.py
+++ extract_frames.py
@@ -14,26 +14,18 @@ def extract_frames(in_dir, out_dir):
 
     for utt_path in in_dir.rglob('*.mp4'):
         cap = load_video(utt_path)
+        frame_dir = out_dir / Path(*Path(os.path.splitext(utt_path)[0]).parts[-3:])
+        mkdir(frame_dir)
         has_frame, frame = cap.read()
         count = 0
         while has_frame:
-            frame_dir = out_dir / Path(*Path(os.path.splitext(utt_path)[0]).parts[-3:])
-            mkdir(frame_dir)
-            frame_path = get_frame_path(out_dir, utt_path, count)
-            cv2.imwrite(frame_path, frame)
+            cv2.imwrite(f'{str(frame_dir)}/frame_{count}.jpg', frame)
             has_frame, frame = cap.read()
             count += 1
 
 
 def mkdir(path):
     Path(path).mkdir(parents=True, exist_ok=True)
-
-
-def get_frame_path(out_dir, utt_path, count):
-    parts = list(utt_path.parts[-3:])
-    filename = f'frame_{count}'
-    parts[2] = filename
-    return out_dir / Path(*parts)
 
 
 if __name__ == "__main__":
