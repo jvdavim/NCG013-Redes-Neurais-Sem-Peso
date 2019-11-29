6bbaa72a65b90fc581ffffcb22ba9f3604f5cd91
Joao Victor Almeida Davim <jvadavim@gmail.com>
2019-11-29 10:00:26 -0300

    Desfaz modificacao do tipo de argumento

--- crop.py
+++ crop.py
@@ -9,9 +9,9 @@ from yolo.face_detection import get_face_frame
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input', type=Path, default='./videos',
+parser.add_argument('--input', type=str, default='./videos',
                     help='path to inputs directory')
-parser.add_argument('--output', type=Path, default='./outputs',
+parser.add_argument('--output', type=str, default='./outputs',
                     help='path to outputs directory')
 args = parser.parse_args()
 
--- luminance.py
+++ luminance.py
@@ -9,9 +9,9 @@ from utils import mkdir
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input', type=Path, default='./videos',
+parser.add_argument('--input', type=str, default='./videos',
                     help='path to inputs directory')
-parser.add_argument('--output', type=Path, default='./outputs',
+parser.add_argument('--output', type=str, default='./outputs',
                     help='path to outputs directory')
 args = parser.parse_args()
 
