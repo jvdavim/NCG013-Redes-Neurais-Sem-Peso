1dc92080f85c23ca537bc3dfdac4931771ac853e
Joao Victor Almeida Davim <jvadavim@gmail.com>
2019-12-01 17:34:19 -0300

    Implementa ultima etapa do pipeline

--- kernel_canvas.py
+++ kernel_canvas.py
@@ -1,17 +1,18 @@
 import cv2
 import sys
+import csv
 import argparse
 import numpy as np
 
-from wisardpkg import KernelCanvas
+import wisardpkg as wsd
 from pathlib import Path
 from utils import mkdir
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--input', type=str, default='./videos',
+parser.add_argument('--input', type=str, default='./dados_proc_3',
                     help='path to inputs directory')
-parser.add_argument('--output', type=str, default='./outputs',
+parser.add_argument('--output', type=str, default='./teste',
                     help='path to outputs directory')
 parser.add_argument('--csv', type=str, default='./data/omg_TrainVideos.csv',
                     help='path to outputs directory')
@@ -19,9 +20,10 @@ args = parser.parse_args()
 
 
 def applyKernelCanvas(frame):
-    numberOfKernels = 10
-    dimension = 2
-    kc = KernelCanvas(
+    numberOfKernels = 1000
+    dimension = len(frame[0])
+
+    kc = wsd.KernelCanvas(
         dimension,               # required
         numberOfKernels,         # required
         bitsBykernel=3,        # optional
@@ -31,17 +33,41 @@ def applyKernelCanvas(frame):
     return kc.transform(frame.astype(int))
 
 
+def get_utt_dir(in_dir, row):
+    return in_dir / row[3] / 'video' / Path(row[4]).with_suffix('')
+
+
 def main(in_dir, out_dir):
     out_dir = Path(out_dir)
     in_dir = Path(in_dir)
 
     mkdir(out_dir)
 
-    with open(args.csv, 'r') as in_csv:
-        for frame_path in in_dir.rglob('*.jpg'):
-            frame = cv2.imread(str(frame_path), 0)
-            out = applyKernelCanvas(frame)
-            print("hello")
+    arousal_ds = wsd.DataSet()
+    valence_ds = wsd.DataSet()
+    emotion_ds = wsd.DataSet()
+
+    with open(args.csv, 'r', newline='\n') as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        for row in reader:
+            # CAPTURAR DIRETORIO DOS FRAMES DA UTTERANCE ATUAL
+            utt_dir = get_utt_dir(in_dir, row)
+            # CAPTURAR AROUSAL, VALENCE e EMOTIONMAXVOTE PARA UTTERANCE ATUAL
+            arousal = row[5]
+            valence = row[6]
+            emotion = row[7]
+            # PARA CADA FRAME, ADICIONAR UM BININPUT NO DATASET COM A LABEL E O KERNEL CANVAS
+            if utt_dir.is_dir():
+                for frame_path in utt_dir.rglob('*.jpg'):
+                    frame = cv2.imread(str(frame_path), 0)
+                    x = applyKernelCanvas(frame)
+                    arousal_ds.add(wsd.BinInput(x), arousal)
+                    valence_ds.add(wsd.BinInput(x), valence)
+                    emotion_ds.add(wsd.BinInput(x), emotion)
+    arousal_ds.save(str(out_dir / 'arousal_ds.wpkds'))
+    valence_ds.save(str(out_dir / 'valence_ds.wpkds'))
+    emotion_ds.save(str(out_dir / 'emotion_ds.wpkds'))
 
 
 if __name__ == '__main__':
