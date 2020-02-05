f173b95ef4fe59b4bf436d676da154597b9223b7
Joao Victor Almeida Davim <jvadavim@gmail.com>
2020-02-05 09:28:40 -0300

    Organiza codigo do arquivo crop.py
    Nome dos argumentos melhorados
    Help de cada argumento melhorado
    Nome de variáveis melhoradas

--- src/crop.py
+++ src/crop.py
@@ -9,44 +9,44 @@ import pandas as pd
 from lib.utils import mkdir, load_yolonet, load_video, crop_face, diff
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--metadata', type=str, default='../data/omg_TrainVideos.csv',
+                    help='Caminho do arquivo.csv com os dados das utterances')
 parser.add_argument('--videos', type=str, default='../data/videos',
                     help='Diretório com videos organizados conforme na challange')
 parser.add_argument('--output', type=str, default='../output/videos',
-                    help='Diretório com frames com as faces recortadas')
-parser.add_argument('--metadata', type=str, default='../data/omg_TrainVideos.csv',
-                    help='Caminho do arquivo csv para treino')
-parser.add_argument('--index', type=str, default='../tmp/crop_index.csv',
-                    help='Caminho do arquivo que armazena registros já treinados')
+                    help='Diretório onde serão salvas as imagens.jpg com as faces recortadas')
+parser.add_argument('--index', type=str, default='../tmp/index.csv',
+                    help='Caminho do arquivo que armazena registros já processados')
 args = parser.parse_args()
 
+metadata = Path(args.metadata)
 videos_dir = Path(args.videos)
-output = Path(args.output)
-train_csv = Path(args.train)
-index_csv = Path(args.index)
+output_dir = Path(args.output)
+index = Path(args.index)
 
 ############################################
 #        RECUPERA ESTADO ONDE PAROU        #
 ############################################
-if train_csv.is_file():
-    full_df = pd.read_csv(train_csv, sep=',')
+if metadata.is_file():
+    full_df = pd.read_csv(metadata, sep=',')
 else:
     print(f'[!] ==> Arquivo de treino nao encontrado')
     sys.exit(1)
-if index_csv.is_file():
-    index_df = pd.read_csv(index_csv, sep=',', names=['video', 'utterance'])
+if index.is_file():
+    index_df = pd.read_csv(index, sep=',', names=['video', 'utterance'])
     df = diff(full_df, index_df, ['video', 'utterance'])
     print(f'[!] ==> Estado recuperado com suceusso')
 else:
     df = full_df
     print(f'[!] ==> Iniciando processamento do zero')
-del full_df, args, parser, train_csv
+del full_df, args, parser, metadata
 ############################################
 
-with open(index_csv, 'a') as index_file:
+with open(index, 'a') as index_file:
     index_writer = csv.writer(index_file)
-    for index, row in df.iterrows():
+    for i, row in df.iterrows():
         utterance = videos_dir / Path(row[3]) / Path('video') / Path(row[4])
-        out_dir = output / Path(row[3]) / Path('video') / Path(row[4]).with_suffix('')
+        out_dir = output_dir / Path(row[3]) / Path('video') / Path(row[4]).with_suffix('')
         mkdir(out_dir)
         net = load_yolonet()
         cap = load_video(utterance)
